80affc43c953d155ba896495f720b61bb4e4879b
Tejas Sai Acha <tejassaiacha@Tejass-MacBook-Pro.local>
2025-08-11 13:18:51 +0530

    updated port

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const app = express()
 const viewsPath = path.join(__dirname, "../templates/views");
 const publicDirectoryPath = path.join(__dirname, '../public');
 const partialsPath = path.join(__dirname, "../templates/partials");
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath);
@@ -97,6 +98,6 @@ app.get('/{*any}', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log("Server is up and running")
+app.listen(port, () => {
+    console.log("Server is up and running at " + port)
 })
